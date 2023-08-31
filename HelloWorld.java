9c120d4379262aaa439d29f737e4e04c606d8669
Vitoria Helen <vitoriahellen3004@gmail.com>
2023-08-31 15:47:54 -0300

    ap01(java): Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+    public static void main (String [] args){
+        //stdin stdout stderr
+        System.out.println("Hello, World");
+    }
+}
+
+// letra maiuscula significa que é uma classe 
+
+//System.out saida padrão 
+
+//ln do print (line)
+
+//Em Java, o compilador produz bytecode é a linguagem que a JVM entende
+//A JVM interpreta bytecode 
