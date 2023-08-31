60e0c35f44971c8da0968141bee6755b7e55cfb0
Vitoria Helen <vitoriahellen3004@gmail.com>
2023-08-31 16:02:55 -0300

    ap01(java): hello world
    - Usando JOpitionPane

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
+//pacote padrão: java.lang, importado automaticamente
+import javax.swing.JOptionPane;
 public class HelloWorld{
     public static void main (String [] args){
         //stdin stdout stderr
-        System.out.println("Hello, World");
+        //System.out.println("Hello, World");
+        JOptionPane.showMessageDialog(null, "Hello, World"); 
     }
 }
 
